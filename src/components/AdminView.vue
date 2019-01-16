a9b22f198ba6496d21be5e2ea793d97476b7e372
Amedeo <corsiamedeo@gmail.com>
2019-01-16 13:25:09 +0000

    added admin table

--- src/components/AdminView.vue
+++ src/components/AdminView.vue
@@ -2,11 +2,57 @@
     <div class="">
         <h3>AdminView</h3>
         <button  class="btn btn-primary" @click="backToHome">Back to home</button>
+        <div class="table_container">
+            <table class="table">
+                <tr>
+                    <th>ID</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                </tr>
+                <tr v-for="user in users">
+                    <td>{{user.id}}</td>
+                    <td>{{user.firstname}}</td>
+                    <td>{{user.lastname}}</td>
+                    <td>{{user.email}}</td>
+                </tr>
+            </table>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    data:function(){
+        return {
+            users:[
+                {
+                    id:'12412',
+                    firstname:'Max',
+                    lastname:'One',
+                    email:'max@one.uk',
+                },
+                {
+                    id:'3526',
+                    firstname:'John',
+                    lastname:'Two',
+                    email:'john@one.uk'
+                },
+                {
+                    id:'7432',
+                    firstname:'Frank',
+                    lastname:'Maybe',
+                    email:'frank@one.uk'
+                },
+                {
+                    id:'9963',
+                    firstname:'Jessica',
+                    lastname:'Always',
+                    email:'jessica@one.uk'
+                }
+            ]
+        }
+    },
     methods:{
         backToHome(){
             this.$router.push('homepage')
@@ -15,5 +61,15 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style scoped>
+    .table_container {
+        width:55%;
+        margin:auto;
+    }
+    .btn{
+        margin-bottom: 40px;
+    }
+    .table{
+        border:solid #18E084 2px;
+    }
 </style>
--- src/components/SellerView.vue
+++ src/components/SellerView.vue
@@ -1,12 +1,53 @@
 <template lang="html">
-    <div class="">
+    <div >
         <h3>Seller View</h3>
         <button class="btn btn-primary" @click="backToHome">Back to home</button>
+        <div class="table_container">
+            <table class="table">
+                <tr>
+                    <th>Seller</th>
+                    <th>Product</th>
+                    <th>Location</th>
+                    <th>Price</th>
+                </tr>
+                <tr v-for="offer in offers">
+                    <td>{{offer.seller}}</td>
+                    <td>{{offer.product}}</td>
+                    <td>{{offer.location}}</td>
+                    <td>{{offer.price}}</td>
+                </tr>
+            </table>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
+    data:function(){
+        return{
+            offers:[
+                {
+                        seller: 'Shell',
+                        product: 'Diesel B7',
+                        location: 'Flörsheim',
+                        price: 85.3,
+                    },
+                    {
+                        seller: 'OF Seller',
+                        product: 'Mogas E5',
+                        location: 'Flörsheim',
+                        price: 85.3,
+                    },
+                    {
+                        seller: 'Shell',
+                        product: 'Mogas E10',
+                        location: 'Karlsruhe',
+                        price: 85.3,
+                    },
+                
+            ]
+        }
+    },
     methods:{
         backToHome(){
             this.$router.push('Homepage')
@@ -15,5 +56,16 @@ export default {
 }
 </script>
 
-<style lang="css">
+<style scoped>
+    .table_container {
+        
+        width:55%;
+        margin:auto;
+    }
+    .btn{
+        margin-bottom: 40px;
+    }
+    .table{
+        border:solid #18E084 2px;
+    }
 </style>
